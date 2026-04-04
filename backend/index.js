cc21ead50d6e9b80c81699cb800957bcad9fd6b2
MohitSingh2002 <mohitsingh11082002@gmail.com>
2026-04-04 20:43:05 +0530

    Dockerize the backend code.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -8,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
-const PORT = process.env.PORT || 5001;
+const PORT = 5001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -16,6 +17,30 @@ app.get('/', (req, res) => {
 
 app.post('/sign-up', (req, res) => {
     res.status(201).json({ message: 'Sign-up successful!' });
+});
+
+// Connect MongoDB
+// Change mongo to localhost if you want to run it locally without Docker
+mongoose.connect("mongodb://mongo:27017/deploystack")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Simple Schema
+const UserSchema = new mongoose.Schema({
+  name: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+
+// Routes
+app.post("/user", async (req, res) => {
+  const user = await User.create({ name: req.body.name });
+  res.json(user);
+});
+
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
 });
 
 app.listen(PORT, () => {
