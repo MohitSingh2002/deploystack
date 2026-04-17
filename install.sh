3da7a35d230d13e43e72e86862604293f2c99d46
MohitSingh2002 <mohitsingh11082002@gmail.com>
2026-04-17 14:34:20 +0530

    Added node.js in install.sh and remove backend container from docker-compose.yml

--- install.sh
+++ install.sh
@@ -89,28 +89,28 @@ echo ""
 echo -e "${GREEN}✅ Docker and Docker Compose installed successfully!${NC}"
 # echo -e "${YELLOW}⚠️  Log out and back in for docker group changes to take effect.${NC}"
 
-# echo -e "${YELLOW}Step 8: Installing latest Node.js...${NC}"
-# curl -fsSL https://deb.nodesource.com/setup_current.x | bash
-# apt-get install -y nodejs
+echo -e "${YELLOW}Step 8: Installing latest Node.js...${NC}"
+curl -fsSL https://deb.nodesource.com/setup_current.x | bash
+apt-get install -y nodejs
 
-# # Verify installation
-# node -v
-# npm -v
+# Verify installation
+node -v
+npm -v
 
-# echo -e "${GREEN}✅ Node.js installed successfully!${NC}"
+echo -e "${GREEN}✅ Node.js installed successfully!${NC}"
 
-echo -e "${YELLOW}Step 8: Installing Nixpacks...${NC}"
+echo -e "${YELLOW}Step 9: Installing Nixpacks...${NC}"
 curl -sSL https://nixpacks.com/install.sh | bash
 echo -e "${GREEN}Nixpacks installed successfully!${NC}"
 
-echo -e "${YELLOW}Step 9: Installing Docker Buildx...${NC}"
+echo -e "${YELLOW}Step 10: Installing Docker Buildx...${NC}"
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/buildx/releases/download/v0.33.0/buildx-v0.33.0.linux-amd64 -o ~/.docker/cli-plugins/docker-buildx
 chmod +x ~/.docker/cli-plugins/docker-buildx
 docker buildx version
 echo -e "${GREEN}Docker Buildx installed successfully!${NC}"
 
-echo -e "${YELLOW}Step 10: Pulling Flutter base image from Docker Hub...${NC}"
+echo -e "${YELLOW}Step 11: Pulling Flutter base image from Docker Hub...${NC}"
 
 docker pull mohitsingh2002/flutter-base:latest
 
